045c1d09b95bd7e2f937e38dcb2ed989b705b72b
casshernsinz <dhall242020outlook.com>
2020-08-31 13:14:42 -0500

    Downloaded NuGet Package Added DATA Layer

--- JobBoard.UI.MVC/Startup.cs
+++ JobBoard.UI.MVC/Startup.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(JobBoard.UI.MVC.Startup))]
-namespace JobBoard.UI.MVC
+namespace IdentitySample
 {
     public partial class Startup
     {
