8573973f58dd8c97eadabb689a43c9a4b5e99754
casshernsinz <dhall242020outlook.com>
2020-08-31 15:24:31 -0500

    Removed [Authorized] from About View in Home Controller
    Added Padding to About and Contact View
    
    Added Url.Action to line 36 in _layout for Logo
    
    Added Html.ActionLink to Nav-Bar in _Layout lines 47-48

--- JobBoard.UI.MVC/Controllers/HomeController.cs
+++ JobBoard.UI.MVC/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace JobBoard.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
