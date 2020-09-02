af6b6a4a269cc5da17ee6ad89fa08029826ae4d9
casshernsinz <dhall242020outlook.com>
2020-09-01 19:58:10 -0500

    Made Classes for each set of MetaData
    Added LastName to Applications "Index"
    
    Created UsersViewModel

--- JobBoard.DATA.MVC/MetaData/JobBoard.MetaData.cs
+++ JobBoard.DATA.MVC/MetaData/JobBoard.MetaData.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JobBoard.DATA.MVC.MetaData
+namespace JobBoard.DATA.MVC//.MetaData
 {
-    class JobBoard
+    public class ApplicationMetadata
     {
         #region Application MetaData
         public int ApplicationId { get; set; }
@@ -17,12 +17,18 @@ namespace JobBoard.DATA.MVC.MetaData
         public int ApplicationStatusId { get; set; }
         public string ResumeFileName { get; set; }
         #endregion
+    }
 
+    public class ApplicationStatusMetadata
+    {
         #region Applcation Status MetaData
         public string StatusName { get; set; }
         public string StatusDescription { get; set; }
         #endregion
+    }
 
+    public class LocationMetadata
+    {
         #region Location MetaData
         public int LocationId { get; set; }
         public string StoreNumber { get; set; }
@@ -30,19 +36,30 @@ namespace JobBoard.DATA.MVC.MetaData
         public string State { get; set; }
         public string ManagerId { get; set; }
         #endregion
+    }
 
+    public class OpenPositionMetadata
+    {
         #region OpenPosition MetaData
         public int PositionId { get; set; }
         #endregion
+    }
 
+    public class PositionMetaData
+    {
         #region Position MetaData
         public string Title { get; set; }
         public string JobDescription { get; set; }
         #endregion
+    }
 
+    public class UserDetailsMetadata
+    {
         #region UserDetails MetaData
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         #endregion
     }
+    
 }
---
+++ JobBoard.UI.MVC/Models/UsersViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace JobBoard.UI.MVC.Models
+{
+    public class UsersViewModels
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
